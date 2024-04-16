6c305b2e093375f4373ef7c7e4e0ea579f427761
git <ignaciosotelo8@gmail.com>
2024-04-16 19:09:46 -0300

    server changes

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const app = express()
 const port = 3030
 
 app.use(express.json())
+app.use(express.static(path.join(__dirname, '../list_movies')))
 
 app.get('/movies', (req, res) => {
     const {search} = req.query
